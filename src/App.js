4f0c26bc817c780c0c0b5730b19099c45eb9748c
narasaken <nazirasakenn@gmail.com>
2023-05-08 23:32:13 +0600

    added todos

--- src/App.js
+++ src/App.js
@@ -6,7 +6,38 @@ import ToDo from "./components/ToDo";
 import Footer from "./components/Footer";
 
 function App() {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([
+    {
+      task: "Write Essay",
+      id: "1",
+      isCompleted: false,
+      isInTrash: false
+    },
+    {
+      task: "One Hour CSS Course Online",
+      id: "2",
+      isCompleted: false,
+      isInTrash: false
+    },
+    {
+      task: "Buy One Way Ticket to San Francisco",
+      id: "3",
+      isCompleted: false,
+      isInTrash: false
+    },
+    {
+      task: "Go to Gym",
+      id: "4",
+      isCompleted: false,
+      isInTrash: false
+    },
+    {
+      task: "Buy Groceries",
+      id: "5",
+      isCompleted: true,
+      isInTrash: false
+    }
+]);
   const [inputTodo, setInputTodo] = useState("");
   const [filter, setFilter] = useState("All");
   const [deletedTodos, setDeletedTodos] = useState([]);
--- src/components/ToDo/todos.jsx
+++
@@ -1,9 +0,0 @@
-const TODOS = [
-    {name: "Write Essay", status: "todo"},
-    {name: "One Hour CSS Course Online", status: "todo"},
-    {name: "Buy One Way Ticket to San Fransico", status: "todo"},
-    {name: "Go to Gym", status: "todo"},
-    {name: "Buy Groceries", status: "done"},
-];
-
-export default TODOS;
