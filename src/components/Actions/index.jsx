da1e77433f89ce5b8c5e9807c4e6db96e7beb9be
narasaken <nazirasakenn@gmail.com>
2023-05-09 00:42:06 +0600

    improved section buttons

--- src/components/Actions/index.jsx
+++ src/components/Actions/index.jsx
@@ -2,7 +2,13 @@ import React, { useState } from "react";
 import "./style.css";
 
 export default function Actions({setFilter, setInputTodo, inputTodo, handleAddTask}) {
+    const [activeFilter, setActiveFilter] = useState("All");
     const [isAddClicked, setAddClicked] = useState(false);
+
+    const handleFilterClick = (filter) => {
+        setFilter(filter);
+        setActiveFilter(filter);
+    };
 
     const handleAddClick = () => {
         setAddClicked(!isAddClicked);
@@ -10,14 +16,12 @@ export default function Actions({setFilter, setInputTodo, inputTodo, handleAddTa
 
     return(
         <div className="actions">
-            <button className="all" onClick={() => setFilter("All")}>All</button>
-            <button className="toDo" onClick={() => setFilter("To Do")}>To Do</button>
-            <button className="done" onClick={() => setFilter("Done")}>Done</button>
-            <button className="trash" onClick={() => setFilter("Trash")}>Trash</button>
+            <button className="all" onClick={() => handleFilterClick("All")} style={{backgroundColor: activeFilter === "All" ? 'rgba(8, 30, 52, 0.42)' : '#F0F0F0'}}>All</button>
+            <button className="toDo" onClick={() => handleFilterClick("To Do")} style={{backgroundColor: activeFilter === "To Do" ? 'rgba(8, 30, 52, 0.42)' : '#F0F0F0'}}>To Do</button>
+            <button className="done" onClick={() => handleFilterClick("Done")} style={{backgroundColor: activeFilter === "Done" ? 'rgba(8, 30, 52, 0.42)' : '#F0F0F0'}}>Done</button>
+            <button className="trash" onClick={() => handleFilterClick("Trash")} style={{backgroundColor: activeFilter === "Trash" ? 'rgba(8, 30, 52, 0.42)' : '#F0F0F0'}}>Trash</button>
     
-            <button onClick={handleAddClick} className="add" style={{
-                backgroundColor: isAddClicked ? 'rgba(0, 0, 0, 0.20)' : 'rgba(0, 0, 0, 0.10)'
-                }}>+</button>
+            <button onClick={handleAddClick} className="add" style={{backgroundColor: isAddClicked ? 'rgba(0, 0, 0, 0.20)' : 'rgba(0, 0, 0, 0.10)'}}>+</button>
                 {isAddClicked ? (
                 <div className="add-window">
                     <p className="add-task">Add New To Do</p>
--- src/components/ToDo/index.jsx
+++ src/components/ToDo/index.jsx
@@ -25,24 +25,6 @@ export default function ToDo({filter, toggleCompleteTask, handleFilter, handleDe
             )}
             </div>
             
-        
-            {/* <button onClick={() => handleDeleteTask(todo.id)} className="trashButton" style={{
-                backgroundColor: isBtnClicked ? 'rgba(0, 0, 0, 0.16)' : 'rgba(0, 0, 0, 0.26)', 
-                marginTop: 6,
-                display: 'flex',
-                alignItems: 'center',
-                }}>
-                <p style={{
-                    fontSize: 28, 
-                    paddingRight: 15, 
-                    paddingLeft: 5, 
-                    paddingTop: "70px",
-                    }}>&#128465;</p>
-                <p style={{
-                    fontSize: 18, 
-                    paddingTop: "76px",
-                    }}>Move to Trash</p>
-            </button>         */}
         </div> 
     );
 }
